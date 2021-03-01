231589eb4560f13c68e26fbfeeac21c560531ea4
Olaf-2137 <79452673+Olaf-2137@users.noreply.github.com>
2021-03-01 21:10:04 +0100

    Create Main.java

---
+++ zadanie 1/Main.java
@@ -0,0 +1,21 @@
+
+package sample;
+
+public class main {
+    public static void main(String[] args) {
+
+        Remote pilot = new Remote();
+        pilot.volume("up");
+        pilot.tv_on();
+        pilot.volume("up");
+
+        pilot.volume("down");
+        pilot.volume("down");
+        pilot.volume("down");
+
+        pilot.channel("up");
+        pilot.tv_on();
+        pilot.channel("down");
+
+    }
+}
